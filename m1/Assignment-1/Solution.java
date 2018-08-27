cfaca1f23117e278120fbb1ac318310ffe9d5cfe
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-08-27 18:32:54 +0530

    assignment-1

---
+++ m1/Assignment-1/Solution.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String name;
+		int rollnumber;
+		name = sc.next();
+		rollnumber = sc.nextInt();
+	System.out.println("Name :"+name+" Roll Number : "+rollnumber);
+	}
+}	
