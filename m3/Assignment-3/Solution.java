d9ed3ad0eaad45b03980f45bbb0b60e65329661f
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-08-29 14:23:17 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m3/Assignment-3/Solution.java
+++ m3/Assignment-3/Solution.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
-/*
+/**.
  Do not modify this main function.
 */
 final class Solution {
-/**
+/**.
  * gcd of two numbers.
  */
     private Solution(){
@@ -15,22 +15,21 @@ final class Solution {
         int n2 = s.nextInt();
         gcd(n1,n2);
     }
-    /*
+    /**.
     Need to write the gcd function and print the output.
-    
     @param      a     {first value }
     @param      b     {second value}
     */
-    public static void gcd(int a, int b){
+    public static void gcd(int a, int b) {
         int gcd = 1;
         if(b < a){
             int temp1 = a;
             a = b;
             b = temp1;
         }
-        for(int i = 1; i<=b; i++){
-            if(b % i == 0 && a % i == 0){
-                if(gcd < i){
+        for(int i = 1; i <= b; i++) {
+            if(b % i == 0 && a % i == 0) {
+                if(gcd < i) {
                     gcd = i;
                 }
             }
