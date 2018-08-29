3aca246bc893647f956462f60a7a333761463227
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-08-29 14:22:04 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m3/Assignment-3/Solution.java
+++ m3/Assignment-3/Solution.java
@@ -1,31 +1,36 @@
-
 import java.util.Scanner;
 /*
-	Do not modify this main function.
-	*/
-public class Solution {
-
-    public static void main(String[] args) {
-
-        Scanner s=new Scanner(System.in);      
+ Do not modify this main function.
+*/
+final class Solution {
+/**
+ * gcd of two numbers.
+ */
+    private Solution(){
+        //pass
+    }
+    public static void main(final String[] args) {
+        Scanner s=new Scanner(System.in);
         int n1 = s.nextInt();
         int n2 = s.nextInt();
         gcd(n1,n2);
-        // System.out.println(gcd);
     }
     /*
-	Need to write the gcd function and print the output.
-	*/
-    public static void gcd(int a,int b){
+    Need to write the gcd function and print the output.
+    
+    @param      a     {first value }
+    @param      b     {second value}
+    */
+    public static void gcd(int a, int b){
         int gcd = 1;
-        if(b<a){
+        if(b < a){
             int temp1 = a;
             a = b;
             b = temp1;
         }
-        for(int i = 1;i<=b;i++){
-            if(b%i == 0 && a%i == 0){
-                if(gcd<i){
+        for(int i = 1; i<=b; i++){
+            if(b % i == 0 && a % i == 0){
+                if(gcd < i){
                     gcd = i;
                 }
             }
