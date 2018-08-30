9e29c08de626003ed8c39c97d3e8e8982c7ce3e3
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-08-30 15:37:58 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m4/Assignment-4/Solution.java
+++ m4/Assignment-4/Solution.java
@@ -6,28 +6,28 @@ public final class Solution {
 /**.
  * Do not modify the main function
  */
-	private static String reverse = "";
-	private Solution() { }
-	/**.
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
-		reverse = reverseString(s);
-		System.out.println(reverse);
-	}
-	/**.
-	 * @param      string  user input
-	 * @return     { revrse of given string }
-	 */
-	public static String reverseString(final String string) {
-		char[] arr = string.toCharArray();
-		int length = arr.length;
-		for (int i = length - 1; i >= 0; i--) {
-			reverse += arr[i];
-		}
-		return reverse;
-	}
+    private Solution() { }
+    private static String reverse = "";
+    /**.
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        reverse = reverseString(s);
+        System.out.println(reverse);
+    }
+    /**.
+     * @param      string  user input
+     * @return     { revrse of given string }
+     */
+    public static String reverseString(final String string) {
+        char[] arr = string.toCharArray();
+        int length = arr.length;
+        for (int i = length - 1; i >= 0; i--) {
+            reverse += arr[i];
+        }
+        return reverse;
+    }
 
 }
