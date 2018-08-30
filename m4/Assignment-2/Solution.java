c2eda86167aca22325585fd832863348b1240a56
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-08-30 12:00:42 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ m4/Assignment-2/Solution.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class Solution {
+	/* Fill the main function to print resultant of addition of matrices*/
+	public static void main(String[] args) {
+		int[][] a,b,c;
+		int rows,cols,rows1,cols1;
+		Scanner sc = new Scanner(System.in);
+		rows = sc.nextInt();
+		cols = sc.nextInt();
+		a = new int[rows][cols];
+		for(int i = 0;i<rows;i++){
+			for(int j = 0;j<cols;j++){
+				a[i][j] = sc.nextInt();
+			}
+		}
+		rows1 = sc.nextInt();
+		cols1 = sc.nextInt();
+		b = new int[rows1][cols1];
+		for(int i = 0;i<rows1;i++){
+			for(int j = 0;j<cols1;j++){
+				b[i][j] = sc.nextInt();
+			}
+		}
+		c = new int[rows][cols];
+		if(rows == rows1 && cols == cols1){
+			for(int i = 0;i<rows1;i++){
+				for(int j = 0;j<cols1;j++){
+					c[i][j] = a[i][j]+b[i][j];
+				}
+			}
+		}
+		else{
+			System.out.println("not valid");
+		}
+		for(int i = 0;i<rows1;i++){
+			for(int j = 0;j<cols1;j++){
+				System.out.print(c[i][j]+" ");
+			}System.out.println( );
+		} 
+	}
+}
