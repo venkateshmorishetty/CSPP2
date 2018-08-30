d487e6416e0ede3f9be7e0436670960acfc11721
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-08-30 12:07:25 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- m4/Assignment-2/Solution.java
+++ m4/Assignment-2/Solution.java
@@ -34,8 +34,8 @@ public class Solution {
 		}
 		for(int i = 0;i<rows1;i++){
 			for(int j = 0;j<cols1;j++){
-				System.out.print(c[i][j]+' ');
-			}System.out.println( );
+				System.out.print(c[i][j]+" ");
+			}System.out.println();
 		} 
 	}
 }
