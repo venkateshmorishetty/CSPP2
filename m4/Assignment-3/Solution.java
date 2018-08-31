7ae2d6ae52fbd85178add4caa33296411c45e1af
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-08-31 17:16:14 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- m4/Assignment-3/Solution.java
+++ m4/Assignment-3/Solution.java
@@ -7,26 +7,25 @@ public class Solution
 {/*
 	Do not modify this main function.
 	*/
-	public static void main(String[] args)
-	{
-		Scanner sc=new Scanner(System.in);
-		int n= Integer.parseInt(sc.nextLine());
-		for(int i=0;i<n;i++){
-			String s=sc.nextLine();
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = Integer.parseInt(sc.nextLine());
+		for(int i = 1; i <= n; i++) {
+			String s = sc.nextLine();
 			String res = binaryToDecimal(s);//Write binaryToDecimal function
 			System.out.println(res);
 		}
 	}
 	public static String binaryToDecimal(String s){
 		Long number = Long.parseLong(s);
-		int res=0;
-		int power=0;
-		while(number!=0){
-			Long rem = number%10;
-			res += rem*Math.pow(2,power);
-			number = number/10;
+		int res = 0;
+		int power = 0;
+		while(number != 0) {
+			Long rem = number % 10;
+			res += rem * Math.pow(2,power);
+			number = number / 10;
 			power += 1;
 		}
-		return res+"";
+		return res + "";
 	}
 }	
