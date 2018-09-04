d31e786771559a599291f8a15a74d2ed435b35b7
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-09-04 19:24:12 +0530

    "check_style_score -> 2 of 2 passed. style score: 0/1 "

--- m8/CodecampList/List.java
+++ m8/CodecampList/List.java
@@ -283,10 +283,11 @@ public class List {
      * @param      item   The item
      */
     public void add(final int index, final int item) {
-        int[] array1 = new int[20];
+        int l = 20;
+        int[] array1 = new int[l];
         array1 = array.clone();
         for (int j = index + 1; j < size + 1; j++) {
-            array[j] = array1[j-1];
+            array[j] = array1[j - 1];
         }
         array[index] = item;
         size += 1;
@@ -325,8 +326,8 @@ public class List {
             // based on the list operation invoke the corresponding method
             switch (tokens[0]) {
                 case "add":
-                if(tokens.length <= 2){
-                    l.add(Integer.parseInt(tokens[1]));    
+                if (tokens.length <= 2) {
+                    l.add(Integer.parseInt(tokens[1]));
                 } else {
                     l.add(Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2]));
                 }
@@ -360,9 +361,10 @@ public class List {
                 case "count":
                 System.out.println(l.count(Integer.parseInt(tokens[1])));
                 case "addAll":
-                int temp[] = new int[tokens.length - 1];
+                int s = tokens.length;
+                int temp[] = new int[s - 1];
                 int temp1 = 0, length = tokens.length;
-                for(int j = 1; j < length; j++){
+                for (int j = 1; j < length; j++){
                     temp[temp1] = Integer.parseInt(tokens[j]);
                     temp1 += 1;
                 }
