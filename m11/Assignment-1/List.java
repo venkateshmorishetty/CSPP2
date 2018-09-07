b8c7260a19ccde8a627ab3705e03d252c8e53f83
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-09-07 17:56:13 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- m11/Assignment-1/List.java
+++ m11/Assignment-1/List.java
@@ -408,7 +408,9 @@ public class List {
                     }
                 break;
                 case "subList":
-                    if (tokens.length != 2) { break; }
+                    if (tokens.length != 2) { 
+                        break;
+                    }
                     String[] arrstring3 = tokens[1].split(",");
                     List object = l.subList(Integer.parseInt(arrstring3[0]),
                             Integer.parseInt(arrstring3[1]));
