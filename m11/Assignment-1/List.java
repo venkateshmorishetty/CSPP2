2b34ea93b8e7993d76fff1dd551b9f4fda2e880c
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-09-07 15:25:35 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- m11/Assignment-1/List.java
+++ m11/Assignment-1/List.java
@@ -234,7 +234,7 @@ public class List {
     public List subList(int start, int end) 
     {
     // write the logic for subList
-        if(start<=0 || end < 0 || start>end ){
+        if(start<0 || end < 0 || start>end || end > size || start == end){
             System.out.println("Index Out of Bounds Exception");
             return null;
         }
