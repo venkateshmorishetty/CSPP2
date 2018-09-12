e24bf0f69a8ae4f730baf9d14aae0055f98e2587
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-09-12 19:08:02 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- m16/bookyourshow/Bookyourshow.java
+++ m16/bookyourshow/Bookyourshow.java
@@ -39,7 +39,7 @@ class Bookyourshow {
 	public void bookAShow(String movie, String date,Patron p ,String[] seats) {
 		patr[size1++] = p;
 		Show show = getAShow(movie, date);
-		if (size == 0 && show == null){
+		if (size == 0 || show == null){
 			System.out.println("No show");
 			return;
 		}
@@ -58,7 +58,7 @@ class Bookyourshow {
 	public void printTicket(String movie, String date,String phno) {
 		int count = 0;
 		Show show = getAShow(movie, date);
-		if( size==0 && show == null) {
+		if( size==0 || show == null) {
 			System.out.println("Invalid");
 		}
 		else {
