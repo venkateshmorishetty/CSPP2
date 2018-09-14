5b74c35c163a1482f4d3326a6e29235eacda0336
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-09-14 19:57:44 +0530

    "check_style_score -> 1 of 4 passed. style score: 0/1 "

--- m17/Shoppingcart/Solution.java
+++ m17/Shoppingcart/Solution.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class Item{
+class Item {
 	String item;
 	int quantity;
 	float price;
@@ -9,90 +9,81 @@ class Item{
 		this.price = price;
 
 	}
-	Item(String item, int quantity){
+	Item(String item, int quantity) {
 		this.item = item;
 		this.quantity = quantity;
 	}
-	public String getitem(){
+	public String getitem() {
 		return this.item;
 	}
-	public int getquantity(){
+	public int getquantity() {
 		return this.quantity;
 	}
-	public double getprice(){
+	public double getprice() {
 		return this.price;
 	}
-	public String display(){
-		return (this.item+" "+this.quantity+" "+this.price);
+	public String display() {
+		return (this.item + " " + this.quantity + " " + this.price);
 	}
-	public String tostring(){
-		return (this.item+" "+this.quantity);
+	public String tostring() {
+		return (this.item + " " + this.quantity);
 	}
 }
-class Shopingcart{
+class Shopingcart {
 	Item[] items_array = new Item[10];
 	int size = 0;
+	int k =1;
 	Item[] list = new Item[10];
 	int size1 = 0;
-	double totalprice,disc;
-	String[] coupons = new String[] {"IND10","IND20","IND30","IND50"};
+	double totalprice, disc;
+	String[] coupons = new String[] {"IND10", "IND20", "IND30", "IND50"};
 	public void addTocatalog(Item item) {
 		items_array[size++] = item;
 	}
-	public void catalog(){
-		for(int i = 0; i < size; i++) {
-			System.out.println(items_array[i].display());	
+	public void catalog() {
+		for (int i = 0; i < size; i++) {
+			System.out.println(items_array[i].display());
 		}
 	}
-	public void addToCart(Item item)
-	{
+	public void addToCart(Item item) {
 		int flag = 0;
-		if(size != 0)
-		{
-			for(int j = 0;j<size1;j++)
-			{
-				if(item.getitem().equals(list[j].getitem())&&item.getquantity()>=list[j].getquantity())
-				{
+		if (size != 0) {
+			for (int j = 0; j < size1; j++) {
+				if (item.getitem().equals(list[j].getitem()) && item.getquantity() >= list[j].getquantity()) {
 					flag = 1;
-					list[j].quantity+=item.quantity;
-					for(int i = 0; i < size; i++)
-					{
-						if(item.getitem().equals(items_array[i].getitem()))
-						{
-						items_array[i].quantity -= item.getquantity();
-						break;
+					list[j].quantity += item.quantity;
+					for (int i = 0; i < size; i++) {
+						if (item.getitem().equals(items_array[i].getitem())) {
+							items_array[i].quantity -= item.getquantity();
+							break;
 						}
 					}
 				}
 			}
-			if(flag == 0)
-			{
-				for(int i = 0; i < size; i++) 
-				{
-					if(item.item.equals(items_array[i].getitem()))
-					{
+			if (flag == 0) {
+				for (int i = 0; i < size; i++) {
+					if (item.item.equals(items_array[i].getitem())) {
 						items_array[i].quantity -= item.getquantity();
 						list[size1++] = item;
 						break;
-						
+
 					}
 				}
 			}
 		}
 	}
 	public void removeFromCart(Item item) {
-		for(int i = 0; i < size1; i++) {
-			if(item.item.equals(list[i].getitem())){
+		for (int i = 0; i < size1; i++) {
+			if (item.item.equals(list[i].getitem())) {
 				list[i].quantity -= item.quantity;
-				if (list[i].quantity == 0){
-					for(int k = i; k < size1; k++) {
-						list[i] = list[i+1];
+				if (list[i].quantity == 0) {
+					for (int k = i; k < size1; k++) {
+						list[i] = list[i + 1];
 					}
 					size1--;
-				}
-				else{
-					for(int j = 0; j < size; j++) {
-						if(item.item.equals(items_array[j].getitem())){
+				} else {
+					for (int j = 0; j < size; j++) {
+						if (item.item.equals(items_array[j].getitem())) {
 							items_array[j].quantity += item.quantity;
 						}
 					}
@@ -101,16 +92,16 @@ class Shopingcart{
 		}
 	}
 	public void show() {
-		for(int i = 0; i < size1; i++) {
+		for (int i = 0; i < size1; i++) {
 			System.out.println(list[i].tostring());
 		}
 	}
-	public double totalAmount(){
-		double cost=0;;
-		for(int i = 0; i < size1; i++) {
-			for(int j = 0; j < size; j++) {
-				if(list[i].getitem().equals(items_array[j].getitem())) {
-					cost += list[i].quantity*items_array[j].price;
+	public double totalAmount() {
+		double cost = 0;;
+		for (int i = 0; i < size1; i++) {
+			for (int j = 0; j < size; j++) {
+				if (list[i].getitem().equals(items_array[j].getitem())) {
+					cost += list[i].quantity * items_array[j].price;
 					break;
 				}
 			}
@@ -120,81 +111,84 @@ class Shopingcart{
 	}
 
 	public void applyCoupon(String coupon) {
-		double val=0;
-		totalprice = totalAmount();
-		for(int i = 0;i<coupons.length;i++){
-			if(coupons[i].equals(coupon)) {
-				val = Integer.parseInt(coupon.charAt(3)+"");
-				disc = (totalprice*(val*0.1));
-				break;
-			}	
+		if (k == 1) {
+			double val = 0;
+			totalprice = totalAmount();
+			for (int i = 0; i < coupons.length; i++) {
+				if (coupons[i].equals(coupon)) {
+					val = Integer.parseInt(coupon.charAt(3) + "");
+					disc = (totalprice * (val * 0.1));
+					break;
+				}
+			}
+			k++;
 		}
 	}
-	public double payableAmount(){
+	public double payableAmount() {
 		totalprice = totalAmount();
 		totalprice -= disc;
-		double tax = totalprice*0.15;
+		double tax = totalprice * 0.15;
 		totalprice = totalprice + tax;
 		return totalprice;
 	}
 	public double getprice(String item) {
-		for(int i = 0; i < size; i++){
-			if(item.equals(items_array[i].getitem())) {
+		for (int i = 0; i < size; i++) {
+			if (item.equals(items_array[i].getitem())) {
 				return items_array[i].price;
 			}
 		}
 		return 0;
 	}
-	public void print(){
-		System.out.println("Name"+"   "+"quantity"+"   "+"Price");
-		for(int i = 0;i<size1;i++){
-			System.out.println(list[i].getitem()+" "+list[i].getquantity()+" "+getprice(list[i].item));
+	public void print() {
+		System.out.println("Name" + "   " + "quantity" + "   " + "Price");
+		for (int i = 0; i < size1; i++) {
+			System.out.println(list[i].getitem() + " " + list[i].getquantity() + " " + getprice(list[i].item));
 		}
-		System.out.println("Total:"+totalAmount());
-		System.out.println("Disc%:"+disc);
-		System.out.println("Tax:"+totalAmount()*0.15);
-		System.out.println("Payable amount: "+payableAmount());
+		System.out.println("Total:" + totalAmount());
+		System.out.println("Disc%:" + disc);
+		System.out.println("Tax:" + totalAmount() * 0.15);
+		System.out.println("Payable amount: " + payableAmount());
 	}
 }
 
 
-public class Solution{
+public class Solution {
 	public static void main(String[] args) {
 		Shopingcart sc = new Shopingcart();
 		Scanner s = new Scanner(System.in);
 		int no_of_lines = s.nextInt();
-		for(int i = 0; i <= no_of_lines; i++){
+		for (int i = 0; i <= no_of_lines; i++) {
 			String[] line = s.nextLine().split(" ");
-			if(line[0].equals("Item")){
+			if (line[0].equals("Item")) {
 				String[] product = line[1].split(",");
-				sc.addTocatalog(new Item(product[0],Integer.parseInt(product[1]),Float.parseFloat(product[2])));
+				sc.addTocatalog(new Item(product[0], Integer.parseInt(product[1]), Float.parseFloat(product[2])));
 			}
-			if(line[0].equals("catalog")){
+			if (line[0].equals("catalog")) {
 				sc.catalog();
 			}
-			if(line[0].equals("add")){
+			if (line[0].equals("add")) {
 				String[] shopping = line[1].split(",");
-				sc.addToCart(new Item(shopping[0],Integer.parseInt(shopping[1])));
+				sc.addToCart(new Item(shopping[0], Integer.parseInt(shopping[1])));
 			}
-			if(line[0].equals("remove")) {
+			if (line[0].equals("remove")) {
 				String[] remove = line[1].split(",");
-				sc.removeFromCart(new Item(remove[0],Integer.parseInt(remove[1])));
+				sc.removeFromCart(new Item(remove[0], Integer.parseInt(remove[1])));
 			}
-			if(line[0].equals("show")){
+			if (line[0].equals("show")) {
 				sc.show();
 			}
-			if(line[0].equals("totalAmount")) {
-				System.out.println("totalAmount: "+sc.totalAmount());
+			if (line[0].equals("totalAmount")) {
+				System.out.println("totalAmount: " + sc.totalAmount());
 			}
-			if(line[0].equals("coupon")) {
+			if (line[0].equals("coupon")) {
 				sc.applyCoupon(line[1]);
 			}
-			if(line[0].equals("payableAmount")){
-				System.out.println("Payable amount: "+sc.payableAmount());
+			if (line[0].equals("payableAmount")) {
+				System.out.println("Payable amount: " + sc.payableAmount());
 			}
-			if(line[0].equals("print")){
+			if (line[0].equals("print")) {
 				sc.print();
 			}
 		}
 	}
-}	
+}
