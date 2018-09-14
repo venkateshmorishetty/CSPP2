8d7ef73e199d15ddac14f0685af0c4d704d97b00
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-09-14 21:40:22 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m17/Shoppingcart/Shoppingcart.java
+++ m17/Shoppingcart/Shoppingcart.java
@@ -103,7 +103,7 @@ class Shopingcart {
                 }
             }
         }
-        totalprice = cost;
+        // totalprice = cost;
         return cost;
     }
     boolean flag = false;
