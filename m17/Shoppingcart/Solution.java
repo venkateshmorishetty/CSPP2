e1ed650f5e377053274a104450ff3fd6c33b39b3
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-09-14 21:42:24 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m17/Shoppingcart/Solution.java
+++ m17/Shoppingcart/Solution.java
@@ -24,18 +24,21 @@ final class Solution {
             String[] line = s.nextLine().split(" ");
             if (line[0].equals("Item")) {
                 String[] product = line[1].split(",");
-                sc.addTocatalog(new Item(product[0], Integer.parseInt(product[1]), Float.parseFloat(product[2])));
+                sc.addTocatalog(new Item(product[0], Integer.parseInt(product[1]),
+                    Float.parseFloat(product[2])));
             }
             if (line[0].equals("catalog")) {
                 sc.catalog();
             }
             if (line[0].equals("add")) {
                 String[] shopping = line[1].split(",");
-                sc.addToCart(new Item(shopping[0], Integer.parseInt(shopping[1])));
+                sc.addToCart(new Item(shopping[0],
+                    Integer.parseInt(shopping[1])));
             }
             if (line[0].equals("remove")) {
                 String[] remove = line[1].split(",");
-                sc.removeFromCart(new Item(remove[0], Integer.parseInt(remove[1])));
+                sc.removeFromCart(new Item(remove[0],
+                    Integer.parseInt(remove[1])));
             }
             if (line[0].equals("show")) {
                 sc.show();
