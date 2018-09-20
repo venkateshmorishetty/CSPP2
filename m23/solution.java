5c62363cda99d9c166b4b2d56254e190376ff4ea
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-09-20 20:11:26 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- m23/solution.java
+++ m23/solution.java
@@ -1,6 +1,7 @@
 import java.util.*;
 import java.io.*;
-class plagorisim {
+import java.lang.*;
+class plagiarism {
 	public HashMap map(File filename){
 		HashMap<String,Integer> hm = new <String,Integer>HashMap();
 		try{
@@ -26,21 +27,24 @@ class plagorisim {
 		}
 		return hm;
 	}
-	public double similarity(HashMap<String,Integer> hash1,HashMap<String,Integer> hash2) {
-		HashMap<String,Integer> hm1,hm2;
-		hm1 = hash1;
-		hm2 = hash2;
+	public double similarity(HashMap<String,Integer> hm1,HashMap<String,Integer> hm2) {
 		double frequencyvector1 = 0,frequencyvector2 = 0;
 		double similarity;
 		int dotproduct = 0;
+		
 		for(int i:hm1.values()) {
 			frequencyvector1 = frequencyvector1 + Math.pow(i,2);
 		}
 		frequencyvector1 = Math.sqrt(frequencyvector1);
+		
+
 		for(int i:hm2.values()) {
 			frequencyvector2 = frequencyvector2 + Math.pow(i,2);
 		}
 		frequencyvector2 = Math.sqrt(frequencyvector2);
+		
+
+
 		for(String i: hm1.keySet()) {
 			if(hm2.containsKey(i)) {
 				dotproduct = dotproduct + hm1.get(i) * hm2.get(i);
@@ -61,10 +65,10 @@ class solution {
 			System.out.println("empty directory");
 			return;
 		}
-		File dir = new File(foldername);;
+		File dir = new File(foldername);
 		File[] filearray = dir.listFiles();
 		HashMap[] hashmaparray = new HashMap[filearray.length];
-		plagorisim p = new plagorisim();
+		plagiarism p = new plagiarism();
 		int temp = 0;
 		long[][] result = new long[filearray.length][filearray.length];
 		
@@ -77,11 +81,13 @@ class solution {
 				result[i][j] = Math.round(p.similarity(hashmaparray[i],hashmaparray[j])*100);
 			}
 		}
+		System.out.print("\t");
 		for(int i = 0; i< filearray.length;i++) {
-			System.out.print(filearray[i]+"\t");
+			System.out.print(filearray[i].toString().split("\\\\")[1]+"\t");
 		}
 		System.out.println();
 		for(int i = 0; i < filearray.length;i++) {
+			System.out.print(filearray[i].toString().split("\\\\")[1]+"\t");
 			for(int j = 0; j < filearray.length; j++) {
 				System.out.print(result[i][j]+"\t");
 			}
