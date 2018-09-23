3820277479016a96e7a80bdef8ac46f438afb1a8
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-09-23 17:18:03 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- m24/Bagofwords/Solution.java
+++ m24/Bagofwords/Solution.java
@@ -27,7 +27,7 @@ class plagiarism {
 				String[] line = str.split(" ");
 				String word = "";
 				for (int i = 0; i < line.length; i++) {
-					word = line[i].replaceAll("[^a-zA-Z0-9_]","").toLowerCase();
+					word = line[i].replaceAll("[^a-zA-Z0-9_]","").replaceAll("."," ").toLowerCase();
 					if (word.length() > 0) {
 						if (hm.containsKey(word)) {
 							hm.put(word, hm.get(word) + 1);
