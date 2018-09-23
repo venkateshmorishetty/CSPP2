6061069fd4fd670d8d820555ce76b0028791f302
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-09-23 23:42:32 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- m24/Stringmatching/Solution.java
+++ m24/Stringmatching/Solution.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 /**
  * Class for stringmatch.
  */
-class Stringmatch{
+class Stringmatch {
 	String str1 = "";
 	String str2 = "";
 	String str = "";
@@ -17,23 +17,23 @@ class Stringmatch{
 	public Stringmatch(final File file1, final File file2) {
 		try {
 			Scanner sc = new Scanner(file1);
-			while(sc.hasNext()){
+			while (sc.hasNext()) {
 				str1 += sc.nextLine();
 			}
 			sc  = new Scanner(file2);
-			while(sc.hasNext()) {
+			while (sc.hasNext()) {
 				str2 += sc.nextLine();
 			}
-			for(int i = 0; i < str2.length(); i++) {
-				for(int j = i + 1; j <= str2.length(); j++) {
-					if(str1.contains(str2.substring(i, j))) {
+			for (int i = 0; i < str2.length(); i++) {
+				for (int j = i + 1; j <= str2.length(); j++) {
+					if (str1.contains(str2.substring(i, j))) {
 						if (str2.substring(i, j).length() > str.length()) {
 							str = str2.substring(i, j);
 						}
 					}
 				}
 			}
-			if(file1.getName().equals(file2.getName())) {
+			if (file1.getName().equals(file2.getName())) {
 				res = 100;
 				System.out.format("%13.1f", res);
 			} else {
@@ -42,20 +42,20 @@ class Stringmatch{
 				res = Math.round(n / d);
 				System.out.format("%13.1f", res);
 			}
-		}catch(Exception e) {
+		} catch(Exception e) {
 			System.out.println("FILE NOT FOUND");
 		}	
 	}
-	public double getResult(){
+	public double getResult() {
 		return res;
 	}		
 }
 /**
  * Class for solution.
  */
-class Solution {
+final class Solution {
 	public static void main(final String[] args) {
-		try{
+		try {
 		Scanner sc = new Scanner(System.in);
 		String foldername = sc.nextLine();
 		File dir = new File(foldername);
@@ -64,17 +64,17 @@ class Solution {
 		String f1 = "";
 		String f2 = "";
         System.out.print("             ");
-		for(int i = 0; i < fileslist.length -1; i++) {
+		for (int i = 0; i < fileslist.length -1; i++) {
 			System.out.print(fileslist[i].getName()+"    ");
 		}
 		System.out.print(fileslist[fileslist.length - 1].getName()+" ");
 		System.out.println();
-		for(int i = 0; i < fileslist.length; i++) {
+		for (int i = 0; i < fileslist.length; i++) {
 			System.out.print(fileslist[i].getName());
-			for(int j = 0; j < fileslist.length; j++) {
+			for (int j = 0; j < fileslist.length; j++) {
 				Stringmatch sm = new Stringmatch(fileslist[i], fileslist[j]);
 				double res = sm.getResult();
-				if(res > maximum && !(fileslist[i].getName().equals(fileslist[j].getName()))) {
+				if (res > maximum && !(fileslist[i].getName().equals(fileslist[j].getName()))) {
 					maximum = res;
 					f1 = fileslist[i].getName();
 					f2 = fileslist[j].getName();
@@ -85,7 +85,7 @@ class Solution {
 			System.out.println();
 		}
 		System.out.println("Maximum similarity is between " +f1+" and "+f2);
-		}catch(Exception e) {
+		} catch(Exception e) {
 			System.out.println("Empty Directory");
 		}
 
